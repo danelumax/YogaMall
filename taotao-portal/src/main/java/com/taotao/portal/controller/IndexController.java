2bd0165eb1d32d4ff578a0eb3ea366de6277a97e
danelumax <474198699@qq.com>
2017-01-21 21:42:44 +0800

    YogaMall : Big Advertise shown on portal and fix httpclient bug

--- taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
+++ taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -8,35 +8,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.taotao.common.pojo.TaotaoResult;
-//import com.taotao.portal.service.ContentService;
+import com.taotao.portal.service.ContentService;
 
 @Controller
 public class IndexController {
 	
-//	@Autowired
-//	private ContentService contentService;
-//
-//	@RequestMapping("/index")
-//	public String showIndex(Model model) {
-//		String adJson = contentService.getContentList();
-//		model.addAttribute("ad1", adJson);
-//		
-//		return "index";
-//	}
-	
+	@Autowired
+	private ContentService contentService;
+
 	@RequestMapping("/index")
-	public String showIndex() {	
+	public String showIndex(Model model) {
+		String adJson = contentService.getContentList();
+		model.addAttribute("ad1", adJson);
+		
 		return "index";
 	}
 	
 	
-	@RequestMapping(value="/httpclient/post", method=RequestMethod.POST) 
-//			produces=MediaType.TEXT_PLAIN_VALUE+";charset=utf-8")
+	@RequestMapping(value="/httpclient/post", method=RequestMethod.POST, 
+			produces=MediaType.TEXT_PLAIN_VALUE+";charset=utf-8")
 	@ResponseBody
 	public String testPost(String username, String password) {
 		String result = "username:" + username + "\tpassword:" + password;
-		System.out.println(result);
+		System.out.println(result); 
 		return "username:" + username + ",password:" + password;
 	}
 	
